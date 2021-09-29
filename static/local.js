01b10ebab9bc6f6ff2c10e034fba5d7551c2df14
notTGY <microcraft47@gmail.com>
2021-09-29 11:29:19 +0300

    https

--- static/local.js
+++ static/local.js
@@ -2,5 +2,5 @@ let DOMAIN
 if (document.location.origin.indexOf('localhost') !== -1) {
   DOMAIN = 'http://localhost:3000'
 } else {
-  DOMAIN = 'http://tedtalkjs.herokuapp.com'
+  DOMAIN = 'https://tedtalkjs.herokuapp.com'
 }
