929bc18b9f89a5d7badf614b47eed9e54fee9b7c
notTGY <microcraft47@gmail.com>
2021-09-29 11:31:51 +0300

    fix qrcode

--- server/createQr.js
+++ server/createQr.js
@@ -3,8 +3,8 @@ const qr = require('qr.js')
 function createQr(id) {
   const qrcode = qr(
     process.env.PORT
-    ?`http://localhost:3000/#${id}`
-    :`http://tedtalkjs.herokuapp.com/#${id}`
+    ?`http://tedtalkjs.herokuapp.com/#${id}`
+    :`http://localhost:3000/#${id}`
   )
 
   return qrcode
